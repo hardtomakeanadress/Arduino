347a455ab1b038c9bab5daa14f34910bbf3d453f
adi-laptop\adi <cursprogramare2015@gmail.com>
2017-07-01 16:16:16 +0300

    no message

--- BatteryCapacityTester/BatteryCapacityTester.ino
+++ BatteryCapacityTester/BatteryCapacityTester.ino
@@ -1,78 +1,77 @@
+//In testing
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 
-#define NUM_SAMPLES 10
-int sum = 0; 
-unsigned char sample_count = 0;
-
-float voltage = 0.0; 
-
-static const byte sensorPin = A6;
-static const byte MOSFET = 2;
+#define MOSFET_Pin 2
+#define Bat_Pin A6
+#define Res_Pin A7
 
 // Set the LCD address to 0x27 for a 16 chars and 2 line display
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-
-
-//new code from here
-#define MOSFET_Pin 2
-
 float Capacity = 0.0; // Capacity in mAh
-float Res_Value = 10.1;  // Resistor Value in Ohm
+float Res_Value = 10.0;  // Resistor Value in Ohm
+float Vcc = 5; // Voltage of Arduino 5V pin ( Mesured by Multimeter )
+float Current = 0.0; // Current in Amp
+float mA=0;         // Current in mA
+float Bat_Volt = 0.0;  // Battery Voltage 
+float Res_Volt = 0.0;  // Voltage at lower end of the Resistor 
+float Bat_High = 4.2; // Battery High Voltage
+float Bat_Low = 3.2; // Discharge Cut Off Voltage
+unsigned long previousMillis = 0; // Previous time in ms
+unsigned long millisPassed = 0;  // Current time in ms
+
+//************************ OLED Display Draw Function *******************************************************
+void draw(void) {
+  lcd.setCursor(0, 0);
+  lcd.print("Volts:");
+  lcd.print(Bat_Volt);
+  lcd.setCursor(0, 1);
+  lcd.print("Capacity:");
+  lcd.print(Capacity, 2);
+}
 
 void setup() {
+  lcd.begin();
+  lcd.backlight();
+  pinMode(MOSFET_Pin, OUTPUT);
+  digitalWrite(MOSFET_Pin, LOW);  // MOSFET is off during the start
+   
+}
   
-    lcd.begin();
-    // Turn on the blacklight and print a message.
-    lcd.backlight();
-    pinMode(MOSFET, OUTPUT);
+//************ Measuring Battery Voltage ***********
+void measureBattery() {
+  Bat_Volt = analogRead(Bat_Pin) * Vcc / 1024.0;
 }
 
+// *********  Measuring Resistor Voltage ***********
+void measureResistor() {
+  Res_Volt = analogRead(Res_Pin) * Vcc / 1024.0;
+}
 
+void makeCalculation() {
+  if ( Bat_Volt > Bat_High) {
+    digitalWrite(MOSFET_Pin, LOW); // Turned Off the MOSFET // No discharge 
+    delay(1000);
+  }
+  else if(Bat_Volt < Bat_Low){
+    digitalWrite(MOSFET_Pin, LOW);
+    delay(1000);
+  }
+  else if (Bat_Volt > Bat_Low && Bat_Volt < Bat_High  ) { // Check if the battery voltage is within the safe limit
+    digitalWrite(MOSFET_Pin, HIGH);
+    millisPassed = millis() - previousMillis;
+    Current = (Bat_Volt - Res_Volt) / Res_Value;
+    mA = Current * 1000.0 ;
+    Capacity = Capacity + mA * (millisPassed / 3600000.0); // 1 Hour = 3600000ms
+    previousMillis = millis();
+    delay(1000); 
+  }
+}
+  
 void loop() {
-  
-  calculateVoltage();
-  printVoltage();
-//  printAmps();
-//  printDischargeTime();
-//  printBatteryCapacity();
-
-}
-
-void calculateVoltage() {
-  while (sample_count < NUM_SAMPLES) {
-        sum += analogRead(sensorPin);
-        sample_count++;
-        delay(20);
-    };
-    
-  voltage = (((float)sum / (float)NUM_SAMPLES * 5.0) / 1024.0 ) * 1.005;
-  sample_count = 0;
-  sum = 0;
-  }
-
-void printVoltage() {
-  lcd.setCursor(0, 0);
-  lcd.print("Volti: ");
-  lcd.print(voltage, 2);
-  }
-
-void printAmps(){
-  lcd.setCursor(0, 0);
-  lcd.print("Amperi: ");
-  }
-
-void printDischargeTime(){
-  lcd.setCursor(0, 0);
-  lcd.print("Run Time: ");
-  }
-
-void printBatteryCapacity() {
-  lcd.setCursor(0, 0);
-  lcd.print("Capacity: ");
-  }
-
-
-/* EOF - Thermometer.ino  */
-
+  measureBattery();
+  measureResistor();
+  makeCalculation(); 
+  draw(); 
+ }    
