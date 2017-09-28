ca12914f048edac2bfb7f978409e3a7049af99ad
DESKTOP-FPHILE9\adi <cursprogramare2015@gmail.com>
2017-09-28 08:54:05 +0300

    no message

--- ESP8266_balcon_raspberrypi_server/ESP8266_balcon_raspberrypi_server.ino
+++ ESP8266_balcon_raspberrypi_server/ESP8266_balcon_raspberrypi_server.ino
@@ -13,40 +13,34 @@ DHT dht(DHTPIN, DHT11, 15);
 WiFiClient client;
 
 void setup() {
-  Serial.begin(115200); 
   dht.begin();
   WiFi.hostname("balconySensor");
   WiFi.begin(ssid, password);
-
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
   }
 }
 
 void readRensors() {
-  h = 50.0;
-  t = 23.5;
-  v = 4.15;
-//  h = dht.readHumidity();
-//  t = dht.readTemperature();
-//  v = analogRead(ADCPIN) * 4.33 / 1024.0;
-  
+  h = dht.readHumidity();
+  t = dht.readTemperature();
+  v = analogRead(ADCPIN) * 4.33 / 1024.0;
+
   if (isnan(h) || isnan(t)) {
     delay(1000);
     return;
   }
 }
-
-void loop() {
-  readRensors();   
+ 
+void sendRequest() {
   
   if (WiFi.status()== WL_CONNECTED) { 
     
     HTTPClient http;    //Declare object of class HTTPClient
-    http.begin("http://192.168.0.103:80/post");      //Specify request destination
+    http.begin("http://192.168.0.120:80/post");      //Specify request destination
     http.addHeader("Content-Type", "application/json");  //Specify content-type header
 
-                       // String postString format should be = "{\"temperature\": 25,\"humidity\": 65,\"voltage\": 3.17}";
+ // String postString format should be = "{\"temperature\": 25,\"humidity\": 65,\"voltage\": 3.17}";
     String postString = "{\"temperature\":";
            postString += t;
            postString += ", \"humidity\":";
@@ -57,10 +51,13 @@ void loop() {
     
     int httpCode = http.POST(postString);   //Send the request
     
-    String payload = http.getString();                  //Get the response payload
-    Serial.println(payload);
+    String responseCode = http.getString();       //Get the response payload
     http.end();  //Close connection
-  }
-  delay(5000);
-//  ESP.deepSleep(600000000,WAKE_RF_DEFAULT);
+  }  
+}
+
+void loop() {
+  readRensors();   
+  sendRequest();
+  ESP.deepSleep(600000000, WAKE_RF_DEFAULT);  
 }
