fdf948e1cdf546c1486f11e9499f93f3812fb2c2
adi stef <hardtomakeanadress@hotmail.com>
2018-10-14 07:13:00 +0300

    add library for nokia lcd and a sketch

---
+++ dht11_nokiaLCD_weatherstation/dht11_nokiaLCD_weatherstation.ino
@@ -0,0 +1,43 @@
+
+
+float h,t;
+#include "U8glib.h"
+#include "DHT.h"
+ 
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+//#define backlight_pin 11
+ 
+DHT dht(DHTPIN, DHTTYPE);
+ 
+U8GLIB_PCD8544 u8g(3, 4, 7, 5, 6);  // CLK=8, DIN=4, CE=7, DC=5, RST=6
+
+void draw(void) {
+
+  u8g.setFont(u8g_font_profont11); 
+  u8g.drawStr(0, 15, "Temperat: ");  
+  u8g.drawStr(0, 35, "Humidity: ");
+  u8g.setPrintPos(55, 15 ); 
+  u8g.print(t, 0);  
+  u8g.drawStr(70, 15, "C ");
+  u8g.setPrintPos(55, 35);
+  u8g.print(h, 0);  
+  u8g.drawStr(70, 35, "% ");
+}
+ 
+void setup(void) {
+  dht.begin();
+//  analogWrite(backlight_pin, 50);  
+}
+ 
+void loop(void) {
+   h = dht.readHumidity(); 
+   t = dht.readTemperature();
+  u8g.firstPage();  
+  do {
+    draw();
+  } while( u8g.nextPage() );
+  
+  delay(500);  
+}
