6a45912c03d1cce45f976d8600f6de17d1b19dad
Adi Stef <hardtomakeanadress@hotmail.com>
2018-11-04 17:58:19 +0200

    edit

--- dht11_nokiaLCD_weatherstation/dht11_nokiaLCD_weatherstation.ino
+++ dht11_nokiaLCD_weatherstation/dht11_nokiaLCD_weatherstation.ino
@@ -5,14 +5,13 @@ float h,t;
 #define DHTPIN 2
 #define DHTTYPE DHT11
 
-#define backlight_pin 11
+//#define backlight_pin 11
  
 DHT dht(DHTPIN, DHTTYPE);
  
 U8GLIB_PCD8544 u8g(3, 4, 7, 5, 6);  // CLK=8, DIN=4, CE=7, DC=5, RST=6
 
 void draw(void) {
-
   u8g.setFont(u8g_font_profont11); 
   u8g.drawStr(0, 15, "Temperat: ");  
   u8g.drawStr(0, 35, "Humidity: ");
@@ -26,7 +25,7 @@ void draw(void) {
  
 void setup(void) {
   dht.begin();
-  analogWrite(backlight_pin, 50);  
+//  analogWrite(backlight_pin, 50);  
 }
  
 void loop(void) {
