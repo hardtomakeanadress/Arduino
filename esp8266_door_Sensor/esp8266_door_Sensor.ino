88754dcbd2fdad657323c992365c3cf9db20cb16
Adi Stef <hardtomakeanadress@hotmail.com>
2018-11-04 20:08:13 +0200

    update library , change shetch

--- esp8266_door_Sensor/esp8266_door_Sensor.ino
+++ esp8266_door_Sensor/esp8266_door_Sensor.ino
@@ -12,13 +12,13 @@ const char *mqtt_client_name = "front_door"; // Client connections cant have the
 const char *mqtt_topic = "doors/front_door";
 //USER CONFIGURED SECTION END//
 
-const byte sensorPin = 2; //esp01 would not boot proper is pin2 is connected to ground at boot time, meaning when the door is closed ...change pin?
+const byte sensorPin = 3; //esp01 would not boot proper is pin2 is connected to ground at boot time, meaning when the door is closed ...change pin? //we used pin3 but needs to change pin function in setup
 byte sensorState;
 
 WiFiClient espClient;
-PubSubClient client(espClient); 
+PubSubClient client(espClient);
 
-//taking the wifi setup in a separate method
+//take the wifi setup in a separate method
 void setup_wifi(){
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED){
@@ -28,27 +28,21 @@ void setup_wifi(){
  
 void setup() {
   WiFi.hostname("FrontDoorSensor");
+  pinMode(sensorPin, FUNCTION_3); //here we change the pin function to GPIO from Serial RX
   setup_wifi();
   pinMode(sensorPin, INPUT_PULLUP);
   sensorState = digitalRead(sensorPin);
   client.setServer(mqtt_server, mqtt_port);
-  Serial.begin(115200);
 }
 
 void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
-    Serial.println("Attempting MQTT connection...");
-    // Attempt to connect
     // If you do not want to use a username and password, change next line to
     // if (client.connect("ESP8266Client")) {
     if (client.connect(mqtt_client_name, mqtt_user, mqtt_pass)) {
-      Serial.println("connected");
     } 
     else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
       // Wait 5 seconds before retrying
       delay(5000);
     }
