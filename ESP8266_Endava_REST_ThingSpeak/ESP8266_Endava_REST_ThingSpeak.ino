743c942c4b1b15d4da625256c0ef08cf9048474b
Adi Stef <nomail@>
2019-12-28 22:31:19 +0200

    library update; add work environmental record

---
+++ ESP8266_Endava_REST_ThingSpeak/ESP8266_Endava_REST_ThingSpeak.ino
@@ -0,0 +1,48 @@
+#include <ESP8266WiFi.h>
+#include "DHT.h"
+#define DHTTYPE DHT11
+#define DHTPIN 2
+//this is the "camera mica" channel
+
+String apiKey        = "ONF60OPAXNXUWG3X";
+const char* ssid     = "warz";
+const char* password = "parola!derezerva";
+const char* server   = "api.thingspeak.com";
+DHT dht(DHTPIN, DHTTYPE);
+WiFiClient client;
+
+void setup() {
+  WiFi.begin(ssid, password);
+  dht.begin();
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+}
+
+void loop() {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  
+  if (client.connect(server,80)) {  // "184.106.153.149" or api.thingspeak.com
+    String postStr = apiKey;
+    postStr +="&field1=";
+    postStr += String(t);
+    postStr +="&field2=";
+    postStr += String(h);
+    postStr += "\r\n\r\n";
+  
+    client.print("POST /update HTTP/1.1\n");
+    client.print("Host: api.thingspeak.com\n");
+    client.print("Connection: close\n");
+    client.print("X-THINGSPEAKAPIKEY: "+apiKey+"\n");
+    client.print("Content-Type: application/x-www-form-urlencoded\n");
+    client.print("Content-Length: ");
+    client.print(postStr.length());
+    client.print("\n\n");
+    client.print(postStr);
+  }
+client.stop();
+
+// thingspeak needs minimum 15 sec delay between updates
+delay(60000);
+}
